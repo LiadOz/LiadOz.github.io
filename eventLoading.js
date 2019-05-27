6cd2393f778afdafb4d9dbf610cc0e98deb2b7da
LiadOz <LiadOzIL@Gmail.Com>
2019-05-27 18:57:37 +0300

    a step towards creating users

--- eventLoading.js
+++ eventLoading.js
@@ -1,17 +1,28 @@
-//the json does not check if the file exist beforehand, could be an issue later.
-
 var userCalendars = [], userLocations = [], availableCalendars = [], availableLocations = []
+
+function userExist(path){
+
+    var http = new XMLHttpRequest();
+    http.open('HEAD', path, false);
+    http.send();
+    return http.status!=404;
+
+}
 //Loads the events into the calendar in from json file.
 function loadEvents(){
 
     const COLORS = [['blue','white'], ['red','white'], ['yellow','black']]
     var username = document.getElementById('userInput').value;
-
+    var path = 'python//web//users//' + username + '.json'
     var events = [], newEvents = [];
     var st ='';
 
-    jQuery.getJSON('python//web//users//' + username + '.json', function(data){
+    if(!userExist(path)){
+        createNewUserMenu(username)
+        return
+    }
 
+    jQuery.getJSON(path, function(data){
     
         userCalendars = data.user_data.sub_calendars;
         userLocations = data.user_data.sub_locations;
@@ -48,7 +59,6 @@ function loadEvents(){
 
         }
 
-        console.log(newEvents)
         updateCalendar(newEvents);
         document.getElementById('description').innerHTML = ''
         showProps()
@@ -57,38 +67,103 @@ function loadEvents(){
         
 }
 
+function createNewUserMenu(username){
+// Creates a menu for inputting a new user.
+    const allEvents = 'python//web//users//all_events.json'
+    jQuery.getJSON(allEvents, function(data){
+        // Reading all available calendars and loctaions
+        availableCalendars = data.aux_data.all_subscriptions
+        availableLocations = data.aux_data.all_locations
+        var str = `<h3>This user does not exist let's create a new user.</h3>
+        <div class="jumbotron">
+        <div class="text-center form-group">
+        <h3>Choose Calendars:</h3>
+        <div class="btn-group btn-group-lg">`;
+        // Showing the calendars options
+        availableCalendars.forEach(element => {
+            str += `<label for="${upAllWords(element)}" class="btn btn-primary">${upAllWords(element)} <input name="inputCalendar" type="checkbox" id="${upAllWords(element)}" class="badgebox"><span class="badge">&check;</span></label>`
+        });
+        str += '</div></div>'
+
+        str += `<div class="text-center">
+        <h3>Choose Locations:</h3>
+        <div class="row">`;
+        str += `<div class="col-md-3">`
+        str += `<div class="btn-group-vertical btn-group-vertical-sm btn-block">`
+        var i = 0
+        // Showing the locations options in four columns
+        availableLocations.forEach(element => {
+            
+            str += `<label for="${upAllWords(element)}" class="btn btn-primary">${upAllWords(element)} <input name="inputLocation" type="checkbox" id="${upAllWords(element)}" class="badgebox"><span class="badge">&check;</span></label>`
+            i++
+            if(i == Math.floor(availableLocations.length/4)){
+                i = 0
+                str += '</div></div>'
+                str += `<div class="col-md-3">`
+                str += `<div class="btn-group-vertical btn-group-vertical-md btn-block">`
+            }
+        });  
+         str += '</div></div>'
+         str += `<button type="submit" class="btn btn-danger btn-block" onclick="createUser()">Submit</button>` // Submit button
+         str += '</div>'
+
+        document.getElementById('description').innerHTML = str
+
+    });
+}
+
+function createUser(){
+    var newCalendars = [], newLocations = [], inputCalendars = Array.from(document.getElementsByName('inputCalendar')), inputLocations = Array.from(document.getElementsByName('inputLocation'))
+    inputCalendars.forEach(element => {
+        if(element.checked == true){
+            newCalendars.push(element.id)
+        }
+    });
+    inputLocations.forEach(element => {
+        if(element.checked == true){
+            newLocations.push(element.id)
+        }
+    });
+    console.log([newCalendars, newLocations])
+}
+
 //Shows information about user in the sidebar.
 function showProps(){
     sideBar = document.getElementById("my-sidebar-content")
 
-    var st = '<a href="#calendarsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item">Calendars</a>'
+    var st = '<a href="#calendarsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item bg-light">Calendars</a>'
     st += '<ul class="collapse list-unstyled" id="calendarsSubmenu">'
     userCalendars.forEach(element => {
-        st+= `<li class="list-group-item list-group-item-action bg-light">${element}</li>`
+        st+= `<li class="list-group-item list-group-item-action bg-light">${upAllWords(element)}</li>`
     });
     st+= '</ul>'
-    st+= '<a href="#locationsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item">Locations</a>'
+    st+= '<a href="#locationsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item bg-light">Locations</a>'
     st += '<ul class="collapse list-unstyled" id="locationsSubmenu">'
     userLocations.forEach(element => {
-        st+= `<li class="list-group-item list-group-item-action bg-light">${element}</li>`
+        st+= `<li class="list-group-item list-group-item-action bg-light">${upAllWords(element)}</li>`
     });
     st+= '</ul>'
 
     //Showing the available subscriptions and calendars
-    st+= '<a href="#avcalendarsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item">Add Calendar</a>'
+    st+= '<a href="#avcalendarsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item bg-light">Add Calendar</a>'
     st += '<ul class="collapse list-unstyled" id="avcalendarsSubmenu">'
+    console.log(availableCalendars)
     availableCalendars.forEach(element => {
-        st+= `<li class="list-group-item list-group-item-action bg-light">${element}</li>`
+        st+= `<li class="list-group-item list-group-item-action bg-light">${upAllWords(element)}</li>`
     });
     st+= '</ul>'
     
-    st+= '<a href="#avlocationsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item">Add Location</a>'
+    st+= '<a href="#avlocationsSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle list-group-item bg-light">Add Location</a>'
     st += '<ul class="collapse list-unstyled" id="avlocationsSubmenu">'
     availableLocations.forEach(element => {
-        st+= `<li class="list-group-item list-group-item-action bg-light">${element}</li>`
+        st+= `<li class="list-group-item list-group-item-action bg-light">${upAllWords(element)}</li>`
     });
     st+= '</ul>'
 
     sideBar.innerHTML = st
+}
+
+function upAllWords(str){
+    return str.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ')
 }
 
